46ead2070e6b21270461cddcbc220503efead694
Matti Paksula <matti.paksula@iki.fi>
2022-04-24 22:29:27 +0300

    good

--- build.sh
+++ build.sh
@@ -1,50 +1,62 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+_shutdown() {
+  echo ""
+  echo "SHUTDOWN"
+
+  docker-compose down --timeout 0
+
+  exit 0
+}
+
+trap _shutdown INT TERM ERR
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 # otherwise multiple networks with the same name will appear when docker-compose is launched in parallel
 docker network create tdx-tools_default || true
 
-docker-compose --ansi never build centos-stream-8-pkg-builder
+(
+  export INPUT_PACKAGE=""
+  docker-compose --ansi never build centos-stream-8-pkg-builder
+)
 
 if [ "${1:-}" = "" ]; then
-  services=$(docker-compose --ansi never config --services)
+  packages="intel-mvp-spr-qemu-kvm intel-mvp-tdx-libvirt intel-mvp-spr-kernel intel-mvp-tdx-tdvf intel-mvp-tdx-guest-grub2 intel-mvp-tdx-guest-shim"
 else
-  services=$@
+  packages=$@
 fi
 
 declare -A pids
-for service in $services; do
-  [ "$service" = "centos-stream-8-pkg-builder" ] && continue
-
+for package in $packages; do
   (
-    exec docker-compose --ansi never run $service | tee "/tmp/$service.log"
-  ) 2>&1 | sed -le "s#\x1B\[[0-9;]\{1,\}[A-Za-z]##g;s#^#$service: #;" &
-  pids[$service]=$!
+    export INPUT_PACKAGE=$package
+    docker-compose --ansi never run centos-stream-8-pkg-builder | tee "/tmp/$package.log"
+    touch build/centos-stream-8/$package/build.done
+  ) 2>&1 | sed -le "s#\x1B\[[0-9;]\{1,\}[A-Za-z]##g;s#^#$package: #;" &
+  pids[$package]=$!
 done
 
 declare -A statuses
-for service in "${!pids[@]}"; do
-  pid=${pids[$service]}
+for package in "${!pids[@]}"; do
+  pid=${pids[$package]}
   set +e
     wait $pid
     code=$?
   set -e
   if [ "$code" = "0" ]; then
-    statuses[$service]=ok
+    statuses[$package]=ok
   else
-    statuses[$service]=fail
+    statuses[$package]=fail
   fi
 done
 
 failed=no
-for service in "${!statuses[@]}"; do
-  status=${statuses[$service]}
-  echo "$service logs in /tmp/$service.log: $status"
+for package in "${!statuses[@]}"; do
+  status=${statuses[$package]}
+  echo "$package logs in /tmp/$package.log: $status"
   if [ "$status" = "fail" ]; then
     failed=yes
-    cat /tmp/$service.log
   fi
 done
 
